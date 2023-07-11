46461df840fbca4e58884b0683b59801755c1017
Cristhian Lima Blanco <cristhianlimablanco@gmail.com>
2023-07-11 16:21:43 -0400

    archivo json con las bebidas simulara una peticion APIRest

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {useEffect, useState} from "react";
 
 function App() {
+  const [drinks, setDrinks] = useState([])
+  useEffect(() => {
+    fetch("json/drinks.json")
+      .then(res => res.json())
+      .then(res => setDrinks(res))
+
+  }, [])
   return (
     <div className="App">
-      Hello World:
+      {drinks.length > 0 && drinks.map(drink => (
+        <div>
+          <h1>{drink.name}</h1>
+          <p>{drink.description}</p>
+          <img src={drink.img} />
+        </div>
+      ))}
     </div>
   );
 }
